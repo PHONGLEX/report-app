c641bf4c8e6de9a4d8650db86a82f3dfd42ba7ee
phonglex <fonglex@gmail.com>
2021-04-19 20:19:35 +0700

    implement chart functionality

--- sales/utils.py
+++ sales/utils.py
@@ -1,6 +1,44 @@
-import uuid
+import uuid, base64
+from customers.models import Customer
+from profiles.models import Profile
+from io import BytesIO
+import matplotlib.pyplot as plt
 
 
 def generate_code():
     code = str(uuid.uuid4()).replace('-', '').upper()[:12]
     return code
+
+def get_salesman_from_id(val):
+    salesman = Profile.objects.get(id=val)
+    return salesman.user.username
+
+def get_customer_from_id(val):
+    customer = Customer.objects.get(id=val)
+    return customer
+
+def get_graph():
+    buffer = BytesIO()
+    plt.savefig(buffer, format='png')
+    buffer.seek(0)
+    image_png = buffer.getvalue()
+    graph = base64.b64encode(image_png)
+    graph = graph.decode('utf-8')
+    buffer.close()
+    return graph
+
+def get_chart(chart_type, data, **kwargs):
+    plt.switch_backend('AGG')
+    if chart_type == '#1':
+        plt.bar(data['transaction_id'], data['price'])
+        print('bar chart')
+    elif chart_type == '#2':
+        labels = kwargs.get('labels')
+        plt.pie(data=data,x='price',labels=labels)
+        print('pie chart')
+    elif chart_type == '#3':
+        print('line chart')
+    else:
+        print('ups... failed to identify the chart type')
+    chart = get_graph()
+    return chart
